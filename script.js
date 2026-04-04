7a765a3fbb55d3a85851a47517ab91f2aa9f44d1
Lachemi BenBokreta Anes <157153180+anexlbk@users.noreply.github.com>
2026-04-04 19:34:00 +0100

    Update PROXY_URL in script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const PROXY_URL = 'https://20.199.160.8.nip.io/webhook/9f3d690b-1623-4a38-ac23-4063f5992b9d/chat'
+const PROXY_URL = 'https://20.199.160.8.nip.io/webhook/72c53231-629d-4d1b-91a7-d4047062f30f/chat'
 
 // ── DOM REFS ──
 const sidebar         = document.getElementById('sidebar');
